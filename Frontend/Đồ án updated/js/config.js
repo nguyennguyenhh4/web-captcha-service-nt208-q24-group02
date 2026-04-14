a0dfd488c1e09f564d28d9d3f615b100f5e6d7a9
24520981-cmd <24520981@gm.uit.edu.vn>
2026-04-14 09:26:34 +0700

    Add files via upload

--- Frontend/Đồ án updated/js/config.js
+++ Frontend/Đồ án updated/js/config.js
@@ -2,6 +2,6 @@ export const CONFIG = {
   pieceSize: 50,
   samplingRate: 16,
   minCanvasDurationMs: 3000,
-  verifyUrl: "/captcha/verify",
+  verifyUrl: "http://127.0.0.1:5000/captcha/verify",
   snapThreshold: 10,
-};
+};z
